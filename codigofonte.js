6b969f0cb6318208b079db402ce3a0d5bf2a8df1
Daniel Previdi <daniel-previdi@hotmail.com>
2026-03-12 21:12:33 -0300

    Update codigofonte.js

--- codigofonte.js
+++ codigofonte.js
@@ -1,3 +1,14 @@
+//Edição github
+let numero1 = 5;
+let numero2 = 3;
+
+let resultado = numero1 + numero2;
+
+console.log("A soma é: " + resultado);
+
+
+//---------------
+
 const bcrypt = require("bcrypt");
 
 // Banco de dados simulado
